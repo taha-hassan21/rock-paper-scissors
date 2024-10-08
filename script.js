772b04e0eb0b9bb421f148ccd794395c70289832
taha <149532993+taha-hassan21@users.noreply.github.com>
2024-10-08 14:11:56 +0530

    Added function to return rock paper scissors randomly

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let x=Math.floor(Math.random() * 3);
+    if (x==0) {
+        return "rock";
+    }
+    else if (x==1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
