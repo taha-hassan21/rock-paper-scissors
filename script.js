d90024df5657f25b736b78417c57413ab8e3616d
taha <149532993+taha-hassan21@users.noreply.github.com>
2024-10-09 14:36:45 +0530

    added the button functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore=0;
+let computerScore=0;
+
 function getComputerChoice() {
     let x=Math.floor(Math.random() * 3);
     if (x==0) {
@@ -11,58 +14,40 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let ans=prompt("Enter a valid choice:");
-
-    if (ans.toLowerCase()!=='rock' && ans.toLowerCase()!=='paper' && ans.toLowerCase()!=='scissors') {
-        return "Enter rock, paper or scissors only";
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice==='rock' && computerChoice==='scissors') {
+        humanScore++;
+        console.log("You win, rock beats scissors");
+    }
+    else if (humanChoice==='scissors' && computerChoice==='rock') {
+        computerScore++;
+        console.log("You lose, rock beats scissors");
+    }
+    else if (humanChoice==='paper' && computerChoice==='rock') {
+        humanScore++;
+        console.log("Your win, paper beats rock");
+    }
+    else if (humanChoice==='rock' && computerChoice==='paper') {
+        computerScore++;
+        console.log("You lose, paper beats rock");
+    }
+    else if (humanChoice==='scissors' && computerChoice==='paper') {
+        humanScore++;
+        console.log("You win, scissors beats paper");
+    }
+    else if (humanChoice==='paper' && computerChoice==='scissors') {
+        computerScore++;
+        console.log("You lose, scissors beats paper");
     }
     else {
-        return ans.toLowerCase();
-    }
-}
-
-
-
-function playGame() {
-    let humanScore=0;
-    let computerScore=0;
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice==='rock' && computerChoice==='scissors') {
-            humanScore++;
-            console.log("You win, rock beats scissors");
-        }
-        else if (humanChoice==='scissors' && computerChoice==='rock') {
-            computerScore++;
-            console.log("You lose, rock beats scissors");
-        }
-        else if (humanChoice==='paper' && computerChoice==='rock') {
-            humanScore++;
-            console.log("Your win, paper beats rock");
-        }
-        else if (humanChoice==='rock' && computerChoice==='paper') {
-            computerScore++;
-            console.log("You lose, paper beats rock");
-        }
-        else if (humanChoice==='scissors' && computerChoice==='paper') {
-            humanScore++;
-            console.log("You win, scissors beats paper");
-        }
-        else if (humanChoice==='paper' && computerChoice==='scissors') {
-            computerScore++;
-            console.log("You lose, scissors beats paper");
-        }
-        else {
-            console.log("It's a draw");
-        }
+        console.log("It's a draw");
     }
 
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
+    // playRound(getHumanChoice(), getComputerChoice());
 
     if (humanScore>computerScore) {
         console.log(`You Won by ${humanScore} to ${computerScore}`);
@@ -75,5 +60,22 @@ function playGame() {
     }
 }
 
+// creating even handlers for each button
 
-console.log(playGame());
+let menu=document.querySelector('#menu');
+
+menu.addEventListener('click', (event)=>{
+    let target=event.target;
+
+    switch(target.id) {
+        case 'rock':
+            playRound('rock', getComputerChoice());
+            break;
+        case 'paper':
+            playRound('paper', getComputerChoice());
+            break;
+        case 'scissors':
+            playRound('scissors', getComputerChoice());
+            break;
+    }
+});
